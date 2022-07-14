ef8b9d5869de7d5125d99e2d75a2542ff448427a
Dongyu Guo <guoguody@hotmail.com>
2022-07-14 01:23:35 -0700

    new push

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 // Require Dependencies
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
+
+//Require routes file
+const routes = require('./routes/index.js');
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+app.use('/', routes);
 
-//Require routes file
-require('./routes/routes')(app);
 
 // Setup listener
 app.listen(PORT, () =>
